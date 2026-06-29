eb332d8e14242df85271611dceaa5975b95334e2
Bhanu2240 <bhanuaitireddy224@gmail.com>
2026-06-29 11:05:51 +0530

    Add root health check endpoint

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ import messageRoutes from "./routes/message.routes.js";
 const app = express();
 
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "REST + Socket.IO Chat API is running 🚀",
+  });
+});
 
 app.use("/users", userRoutes);
 app.use("/messages", messageRoutes);
